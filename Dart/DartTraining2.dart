3fe8b56d79b2744a041e737844c86d6f12a20cb4
jacques-swanepoel <jacques@swanepoel.ca>
2024-06-20 19:46:32 -0400

    Adding sample file

---
+++ Dart/DartTraining2.dart
@@ -0,0 +1,15 @@
+void main() {
+    var x = 5;
+    var y = 20;
+    var sum;
+    sum = x + y;
+    print(sum);
+
+    int xx = 5;
+    double yy = 3.14;
+    print(xx + yy);
+    print(xx - yy);
+    print(xx * yy);
+    print(xx / yy);
+
+}
