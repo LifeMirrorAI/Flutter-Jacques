a8ef672beb386b77e49d2bb6a046b8f3de7a0ae8
jacques-swanepoel <jacques@swanepoel.ca>
2024-06-20 20:11:07 -0400

    Training Chapter 2

--- Dart/DartTraining2.dart
+++ Dart/DartTraining2.dart
@@ -12,4 +12,20 @@ void main() {
     print(xx * yy);
     print(xx / yy);
 
+    String s1 = 'Hello';
+    String s2 = 'World';
+    String s3 = s1 + ', ' + s2 + '!'; // Hello, World!
+    String s4 = '$s1, $s2!';
+    print(s3);
+    print(s4);
+
+    bool isOn = true;
+    bool isOff = false;
+    
+    var z1;
+    print(z1);
+    var z2 = null;
+    print(z2);
+
+
 }
